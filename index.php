d297418671e28dd2333645b85a68c25be230a2ab
taaviprei <prei.taavi@gmail.com>
2020-04-24 09:17:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Cuprum:ital@1&display=swap" rel="stylesheet">
     <title>Main</title>
